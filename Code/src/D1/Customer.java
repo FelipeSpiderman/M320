45e110f74d78103400c3c94d422191f710a18aed
simonzh123 <simon.deuber@edu.tbz.ch>
2025-08-26 08:31:35 +0200

    Exercise D1 - Bank

---
+++ Code/src/D1/Customer.java
@@ -0,0 +1,15 @@
+package D1;
+
+public class Customer {
+    private final String firstName;
+    private final String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getDisplayName() { return firstName + " " + lastName; }
+}
