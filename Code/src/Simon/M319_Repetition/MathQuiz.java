aef522e70b7ab94dddf19ff3603c6a0f646d7263
simonzh123 <simon.deuber@edu.tbz.ch>
2025-09-02 08:54:35 +0200

    D1 | Imporvements Display operation details

---
+++ Code/src/Simon/M319_Repetition/MathQuiz.java
@@ -0,0 +1,25 @@
+package Simon.M319_Repetition;
+
+import java.util.*;
+
+public class MathQuiz {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random randomNumber = new Random();
+        int correct = 0;
+
+        long start = System.currentTimeMillis();
+        for (int i = 1; i <= 10; i++) {
+            int a = randomNumber.nextInt(10) + 1;
+            int b = randomNumber.nextInt(10) + 1;
+            int result = a + b;
+            System.out.print("Aufgabe " + i + ": " + a + " + " + b + " = ");
+            int answer = in.nextInt();
+            if (answer == result) correct++;
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("Richtig: " + correct + "/10");
+        System.out.println("Zeit: " + (end - start) / 1000 + " Sekunden");
+    }
+}
